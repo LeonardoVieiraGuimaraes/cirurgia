b2a44759320330c9ab16bd066c507d4d4eb1eda4
LEONARDO VIEIRA GUIMARAES <42416757+LeonardoVieiraGuimaraes@users.noreply.github.com>
2022-11-15 11:59:31 -0300

    post

--- cirurgia/settings.py
+++ cirurgia/settings.py
@@ -82,8 +82,13 @@ WSGI_APPLICATION = "cirurgia.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('postgres'),
+        'USER': os.getenv('hosmkojhbe'),
+        'PASSWORD': os.getenv('I8KKWH5P60C27H34$'),
+        'HOST': os.getenv('appcirurgiasegura-server.postgres.database.azure.com'),
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'}
     }
 }
 
