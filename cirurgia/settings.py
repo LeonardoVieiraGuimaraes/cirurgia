08f1805d7538431e37741dd9310efdae361ae2cd
LEONARDO VIEIRA GUIMARAES <42416757+LeonardoVieiraGuimaraes@users.noreply.github.com>
2022-11-15 11:13:27 -0300

    pront 02

--- cirurgia/settings.py
+++ cirurgia/settings.py
@@ -80,25 +80,25 @@ WSGI_APPLICATION = "cirurgia.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv('postgres'),
-#         'USER': os.getenv('hosmkojhbe'),
-#         'PASSWORD': os.getenv('I8KKWH5P60C27H34$'),
-#         'HOST': os.getenv('appcirurgiasegura-server.postgres.database.azure.com'),
-#         'PORT': '5432',
-#         'OPTIONS': {'sslmode': 'require'}
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('postgres'),
+        'USER': os.getenv('hosmkojhbe'),
+        'PASSWORD': os.getenv('I8KKWH5P60C27H34$'),
+        'HOST': os.getenv('appcirurgiasegura-server.postgres.database.azure.com'),
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'}
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
